924fcf748764761c2ecb425bd7ae1b5120e3a643
HIlight3R <hilight3r7@gmail.com>
2020-06-26 19:51:21 +0300

    Added base admin functions, customized control panel

--- coins/admin.py
+++ coins/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CategoryUser(admin.ModelAdmin):
+    list_display = ("login", "name", "balance")
+    list_filter = ("login", "name", "balance")
+    readonly_fields = ("password",)
+    fieldsets = (
+        ("Основная информация", {
+            "fields": (("name", "balance"),)
+        }),
+        ("Данные для входа", {
+            "fields": (("login", "password"),)
+        })
+    )
+
+
+admin.site.site_title = "Администрирование HEXYZ"
+admin.site.site_header = "Админ-панель HEXYZ"
--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """Class of user"""
+    name = models.CharField("Имя", max_length=30)
+    login = models.SlugField("Логин", max_length=64, unique=True)
+    password = models.SlugField("Хэш пароля")
+    balance = models.PositiveIntegerField("Баланс", default=0)
+
+    def __str__(self):
+        return self.login
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
