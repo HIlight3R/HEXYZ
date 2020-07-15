13120932604198b04ba5f55c9f1cbb460e3cd221
HIlight3R <hilight3r7@gmail.com>
2020-07-15 20:12:39 +0300

    Добавлена система переводов, очищен и оптимизирован код

--- coins/views.py
+++ coins/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Sum
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 
 from .models import Wallet
@@ -13,3 +14,29 @@ class WalletsView(View):
         totals = wallets.aggregate(Sum('balance'))
         total = totals.get('balance__sum')
         return render(request, "coins/wallets.html", {"wallet_list": wallets, "total_sum": total})
+
+    def post(self, request):
+        try:
+            sender = Wallet.objects.get(id=request.user.id)
+            receiver = Wallet.objects.get(id=int(request.POST.get("id")))
+        except:
+            return HttpResponse(status=400)
+        if sender is not None and receiver is not None and sender != receiver:
+            if request.POST.get("amount", False):
+                amount = float(request.POST.get("amount"))
+                if amount <= float(sender.balance):
+                    sb = float(sender.balance)
+                    rb = float(receiver.balance)
+                    sb -= amount
+                    rb += amount
+                    sender.balance = sb
+                    receiver.balance = rb
+                    sender.save()
+                    receiver.save()
+                else:
+                    return HttpResponse(status=400)
+            else:
+                return HttpResponse(status=400)
+        else:
+            return HttpResponse(status=400)
+        return redirect('./')
